dedb00e3e969387770aa301155d36a855f555f92
boomathi123 <boomathinataj2006@gmail.com>
2025-01-27 12:30:21 +0530

    first commit

---
+++ task5(e).jsx
@@ -0,0 +1,15 @@
+
+import React from 'react'
+
+const Header = () => {
+     
+    return (
+    <div>
+        <h1>
+            Welcome to React Class
+        </h1>
+    </div>
+  )
+}
+
+export default Header
