fdd488fb1052eaf574ef8822c4f54c3241d782ed
boomathi123 <boomathinataj2006@gmail.com>
2025-01-27 11:25:05 +0530

    first commit

---
+++ task5(m).js
@@ -0,0 +1,14 @@
+/*let num =7;
+switch(num%2){
+    case 0:
+        console.log("even numer");
+        break;
+    case 1:
+        console.log("odd number");
+    default:
+        console.log("invalid number");    
+
+
+}
+/*
+
